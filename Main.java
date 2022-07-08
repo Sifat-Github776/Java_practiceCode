9c92116865ad77c3d06969aee9bc200562e2ad43
Sifat-Github776 <89438529+Sifat-Github776@users.noreply.github.com>
2022-07-08 15:44:28 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+// import java.util.*;
+
+// class Main {
+//     public static void main(String[] args) {
+//         int m = 10, n = 10;
+//         System.out.println(m++ / ++n * n-- / --m); 
+//     }
+// }
+
+class Main {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=5;i++) {
+            for( j=1;j<=5;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+       
+    }
+}
